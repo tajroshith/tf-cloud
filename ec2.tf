d32a9512d08891c5ff0c9cf3cfd7eba8c56e192f
tajroshith <tajroshith@gmail.com>
2024-01-27 22:34:56 +0530

    Updated ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "webserver" {
   ami = var.ami_id
   instance_type = var.instance_type
-  subnet_id = aws_route_table.tf-rtb-public[0].id
+  subnet_id = aws_subnet.tf-public-subnets[0].id
   key_name = var.key_name
   tags = merge(var.tags, {
     "Name" = "Webserver"
